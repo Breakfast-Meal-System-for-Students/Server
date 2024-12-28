aae38d82eb1f8c052c39611b8d40c6dcc4503d51
21620023k <dangkhoa3348@gmail.com>
2024-12-28 11:01:44 +0700

    add binlling value isopentoday

--- BMS.BLL/Models/Responses/OpeningHour/GetOpeningHoursForShopResonse.cs
+++ BMS.BLL/Models/Responses/OpeningHour/GetOpeningHoursForShopResonse.cs
@@ -12,6 +12,7 @@ namespace BMS.BLL.Models.Responses.OpeningHour
         public Guid Id { get; set; }
         public WeekDay day { get; set; }
 
+        public bool isOpenToday { get; set; } = true;
         public int from_hour { get; set; }
         public int to_hour { get; set; }
         public int from_minute { get; set; }
