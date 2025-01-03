572fe39060db4aab0d26be324fc00163dfbcd97f
21620023k <dangkhoa3348@gmail.com>
2025-01-03 22:19:09 +0700

    add lnt lat

--- BMS.BLL/Models/Responses/University/UniversityResponse.cs
+++ BMS.BLL/Models/Responses/University/UniversityResponse.cs
@@ -13,6 +13,8 @@ namespace BMS.BLL.Models.Responses.University
         public string Address { get; set; } = null!; // University address
         public string EndMail { get; set; } = null!; // Email domain for university students
 
+        public double? Lng { get; set; } // Longitude of the university's location
+        public double? Lat { get; set; } // Latitude of the university's location
         public string IdStudentFormat { get; set; } = null!; // Format of student IDs
         public string Abbreviation { get; set; } = null!; // Short form of the university name
     }
